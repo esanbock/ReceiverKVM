4025aa8ea6e83e1f86b6cbb86e2c8dca57a60c71
esanbock <douglas@esanbock.com>
2018-12-25 12:57:52 -0600

    switching to raw TCP instead of telnet.   From https://github.com/pklaus/Marantz-SR5009/blob/master/sr5009.py

--- MarantzReceiver.py
+++ MarantzReceiver.py
@@ -1,21 +1,55 @@
 import Receiver
-import telnetlib
+import logging
+import time
+import select
+import socket
 
+logger = logging.getLogger(__name__)
 
 class marantz_receiver(Receiver.receiver):
 
-    def __init__(self, ip):
+    TCP_PORT = 23
+    BUFFER_SIZE = 1024
+    LINE_TERMINATOR = '\r'
+    TIMEOUT = 1
+    WAIT_AFTER_SEND = 1
+
+    def __init__(self, hostname):
         print("connecting...")
-        self.conn = telnetlib.Telnet(ip)
-        intro = self.conn.read_eager()
-        print(intro)
-        pow = self.get_power()
+        self.hostname = hostname
+        self.conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.conn.connect((hostname, self.TCP_PORT))
+
+#        intro = self.receive()
+#        print(intro)
+
+    def send(self, cmd):
+        cmd = cmd.rstrip(self.LINE_TERMINATOR) + self.LINE_TERMINATOR
+        send_data = bytes(cmd, 'ascii')
+        logger.debug('sending: {}'.format(repr(send_data)))
+        self.conn.send(send_data)
+        time.sleep(self.WAIT_AFTER_SEND)
+
+    def receive(self, timeout=TIMEOUT):
+        ready = select.select([self.conn], [], [], timeout)
+        if not ready[0]:
+            raise Exception('Not ready to read data in the specified timeout time {} s'.format(timeout))
+        data = self.conn.recv(self.BUFFER_SIZE)
+        logger.debug('received: {}'.format(repr(data)))
+        data = data.decode('ascii').strip(self.LINE_TERMINATOR)
+        print("parsed " + data)
+        return [line.strip() for line in data.split(self.LINE_TERMINATOR)]
 
     def change_source(self, source):
         print("changing source to " + source)
 
     def get_power(self):
-        self.conn.write("PW?\r".encode("ascii"))
-        powerstatus = self.conn.read_all()
+        self.send('PW?')
+        powerstatus = self.receive()
         print(powerstatus)
-        return powerstatus
+        if "PWON" in powerstatus[0]:
+            return 1
+        if "PWOFF" in powerstatus[0]:
+            return 0
+
+        raise Exception("I don't understand the response")
