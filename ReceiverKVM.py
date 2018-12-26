a9060f1044fb12c65ca1aefd31826f9df663cb5f
esanbock <douglas@esanbock.com>
2018-12-25 21:14:59 -0600

    making parameters command-line based

--- ReceiverKVM.py
+++ ReceiverKVM.py
@@ -1,16 +1,22 @@
 import sys
 import MarantzReceiver
 import PyUSBFinder
+from optparse import OptionParser,  OptionGroup
 
 def main(argv):
     print("yo")
+
+    options, args = processCommandLine(argv)
+    hostname = args[0]
+    productid = int(args[1],16)
+    vendorid = int(args[2],16)
 
     finder = PyUSBFinder.pyusb_finder()
     finder.enumerateall()
     while True:
         print("waiting for device")
-        if finder.wait_for_device(0x204,0x6025) is True:
-            r = MarantzReceiver.marantz_receiver("192.168.1.131")
+        if finder.wait_for_device(productid,vendorid) is True:
+            r = MarantzReceiver.marantz_receiver(hostname)
             powerstatus = r.get_power()
             print("power status = " + str(powerstatus))
             if powerstatus == 1:
@@ -20,9 +26,20 @@ def main(argv):
                 print("receiver is off")
             r.disconnect()
             print("hanging out until device disappears")
-            finder.wait_for_device_disappear(0x204,0x6025)
+            finder.wait_for_device_disappear(productid,vendorid)
 
     print("done")
+
+def processCommandLine(argv):
+    parser = OptionParser("ReceiverKVM <receiveraddress> <vendorid> <productid>")
+
+    options, args = parser.parse_args()
+
+    if len(args) != 3:
+                parser.print_help()
+                parser.error("invalid number of arguments")
+
+    return options, args
 
 if __name__ == "__main__":
     main(sys.argv)
